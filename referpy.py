3ac52675013e541aaa014c92333beff11bec376e
lsj <lsj9383@163.com>
2018-01-23 21:25:57 +0800

    update icon

--- referpy.py
+++ referpy.py
@@ -23,7 +23,7 @@ def main():
         except Exception as e:  raise e
         else:
             print("【APA】："+mapper["APA"])
-            print("【APGBT7714A】："+mapper["GBT7714"])
+            print("【GBT7714A】："+mapper["GBT7714"])
             print("【MLA】："+mapper["MLA"])
             return
     
@@ -39,9 +39,9 @@ def main():
         try:
             mapper = services.retryWrapper(scholar.searchRef, 3)(title, debug)  # 重试3次
         except Exception as e:       # 出现异常，将错误进行记录
-            files["apa.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)))
-            files["gbt7714.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)))
-            files["mla.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)))
+            files["apa.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)).encode("utf8"))
+            files["gbt7714.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)).encode("utf8"))
+            files["mla.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)).encode("utf8"))
             haveExceptionNote = True
         else:
             files["apa.txt"].write("[{0}] {1} \n".format(lineNumber, mapper['APA']).encode("utf8"))
