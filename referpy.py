48489e92842d448a8616eac23247fe9cf80976f6
lsj <lsj9383@163.com>
2018-01-23 17:00:32 +0800

    init master

---
+++ referpy.py
@@ -0,0 +1,61 @@
+# _*_ coding:utf-8 _*_
+import sys
+
+def main():
+    import scholar
+    import os
+    import time
+    import services
+
+    debug = False
+    if(len(sys.argv) == 1):
+        raise Exception("【异常】 没有输入期望检索的目录和文件")
+    if(len(sys.argv)==3 and (sys.argv[2]=="-debug" or sys.argv[2]=="-d" or \
+                            sys.argv[2]=="-DEBUG" or sys.argv[2]=="-D")):
+        debug = True
+
+    # 快速查询模式
+    if(sys.argv[1].startswith("paper:")):
+        try:
+            title = sys.argv[1][6:]
+            print("search \"{0}\"".format(title))
+            mapper = services.retryWrapper(scholar.searchRef, 3)(title, debug)  # 重试3次
+        except Exception as e:  raise e
+        else:
+            print("【APA】："+mapper["APA"])
+            print("【APGBT7714A】："+mapper["GBT7714"])
+            print("【MLA】："+mapper["MLA"])
+            return
+    
+    # 目录/文件查询模式
+    if ((not os.path.isfile(sys.argv[1])) and (not os.path.isdir(sys.argv[1]))):
+        raise Exception("【异常】 没有找到该目录或文件：\"{0}\"".format(sys.argv[1]))
+    lineNumber = 0
+    haveExceptionNote=False
+    files = services.openall("wb", "apa.txt", "gbt7714.txt", "mla.txt")
+    for title in services.paperTitleItera(sys.argv[1]):
+        lineNumber += 1
+        print("search \"{0}\"".format(title))
+        try:
+            mapper = services.retryWrapper(scholar.searchRef, 3)(title, debug)  # 重试3次
+        except Exception as e:       # 出现异常，将错误进行记录
+            files["apa.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)))
+            files["gbt7714.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)))
+            files["mla.txt"].write("[{0}] {1} \n".format(lineNumber, str(e)))
+            haveExceptionNote = True
+        else:
+            files["apa.txt"].write("[{0}] {1} \n".format(lineNumber, mapper['APA']).encode("utf8"))
+            files["gbt7714.txt"].write("[{0}] {1} \n".format(lineNumber, mapper['GBT7714']).encode("utf8"))
+            files["mla.txt"].write("[{0}] {1} \n".format(lineNumber, mapper['MLA']).encode("utf8"))
+        services.flushall(files)
+        time.sleep(0.01)
+    services.closeall(files)
+    if(haveExceptionNote):
+        raise Exception("【注意】某些论文文献格式获取存在异常，请打开文件检查")
+            
+
+
+if __name__ == "__main__":
+    try:    main()
+    except: print(sys.exc_info()[1])
+    else:   print("【成功】")
---
+++ services.py
@@ -0,0 +1,52 @@
+# _*_ coding:utf-8 _*_
+
+# 多个文件打开
+def openall(mod, *files):
+    mapper={}
+    for fil in files:
+        mapper[fil] = open(fil, mod)
+    return mapper
+
+# 刷新多个文件
+def flushall(files):
+    for key, value in files.items():
+        value.flush()
+
+# 关闭多个文件
+def closeall(files):
+    for key, value in files.items():
+        value.close()
+
+# 重试方法封装器.可以用装饰器代替
+def retryWrapper(method, times):
+    def retryMethod(*args, **kwargs):
+        nonlocal times
+        while(times):
+            times-=1
+            try:
+                return method(*args, **kwargs)
+            except Exception as e:
+                if(times==0):   raise Exception("【异常】\"{0}\".若无法解决请手动查询该文献，或是和本人联系:asirlu@foxmail.com".format(str(e))) # 不可重试，抛出异常
+        
+    return retryMethod
+
+# 论文标题迭代器,根据路径是目录或是文件才去不同的迭代方式
+def paperTitleItera(path):
+    import os
+    import sys
+    if(os.path.isfile(path)):
+        papersFile = open(path, "rb")
+        for titleBytes in papersFile:             # 遍历文件
+            title = titleBytes.decode("utf8").strip()
+            if(len(title)==0):   continue
+            yield title
+        papersFile.close()
+        return
+    else:
+        for fileName in os.listdir(path):    # 遍历文件夹
+            if(os.path.isdir(path+"/"+fileName)):continue
+            (title, suffix) = os.path.splitext(fileName)    # 除去后缀
+            title = title.strip()
+            if(len(title)==0):   continue
+            yield title
+        return
